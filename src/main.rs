762394a711a5a53b7e40ee1055d8e56baa3c513f
darcy <darcy3057@gmail.com>
2023-02-14 19:26:44 +1100

    Add args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+mod args;
+
+use clap::Parser;
+
+use args::Args;
 use lingdocs::run;
 
 fn main() {
-    run("./example").expect("Failed");
+    let args = Args::parse();
+
+    run(&args.dir.unwrap_or(".".to_string())).expect("Failed");
 }
